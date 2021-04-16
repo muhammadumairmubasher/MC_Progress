b5e1d4f5b394a1010ff5fb28f6b66d083623d633
MUHAMMAD UMAIR MUBASHER <Muhamadumair7445@gmail.com>
2021-04-16 07:07:50 +0500

    Multimedia Paractice work

--- ToDoTask07Lec09/app/src/main/java/org/umairmubasher/todotask07lec09/MainActivity.java
+++ ToDoTask07Lec09/app/src/main/java/org/umairmubasher/todotask07lec09/MainActivity.java
@@ -3,6 +3,8 @@ package org.umairmubasher.todotask07lec09;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView=findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://"+getPackageName()+"/" + R.raw.kids);
+        MediaController mediaController=new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
     }
 }
